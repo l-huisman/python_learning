29831bfcad5f322f98462ecb36eebf6ac1c2bb79
l-huisman <huisman.l@yahoo.com>
2023-02-18 11:18:51 +0100

    If statements in python

---
+++ if.py
@@ -0,0 +1,12 @@
+x = 1
+y = 1
+if(x == 1):
+    print('x is 1')
+elif(y == 1):
+    print('y is 1')
+else:
+    print('x and y are not 1')
+if(x == 1 and y == 1):
+    print('x and y are 1')
+if(x == 1 or y == 1):
+    print('x or y is 1')
