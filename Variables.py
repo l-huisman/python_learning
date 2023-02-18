df539e1bda7448898f9ca4ed0b5c8e7bd834fca8
l-huisman <huisman.l@yahoo.com>
2023-02-18 11:08:18 +0100

    Variables in python

---
+++ Variables.py
@@ -0,0 +1,12 @@
+# String variable
+string = 'Hello World!'
+# Integer variable
+number = 1
+# Float variable
+float = 1.5
+# Boolean Variable
+boolean = True
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
